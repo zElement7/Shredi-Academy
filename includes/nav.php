8c0dc0a92824a775f7c7ad60f154f7201c3b87be
Zach <zachary.clement7@gmail.com>
2024-02-13 20:20:10 -0700

    Test

---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href=".">Home</a></li>
+        <li><a href="./profile.php">Profile</a></li>
+        <li><a href="./workouts.php">Workouts</a></li>
+        <li><a href="./progress.php">Progress</a></li>
+        <li><a href="./exercise.php">Exercise</a></li>
+    </ul>
+</nav>
