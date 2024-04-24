41e80fa258d3bddf260aa7058788a5449be35bae
Zach <zachary.clement7@gmail.com>
2024-04-24 01:38:34 -0600

    small changes

--- profile.php
+++ profile.php
@@ -35,7 +35,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>User Profile</title>
+        <title>Profile</title>
         <link rel="stylesheet" href="./css/style.css" type="text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -45,14 +45,14 @@
         <?php include_once("includes/nav.php") ?>
         <h1>Profile Page</h1>
         <div class="contentWrapper">
-            <h1>Profile</h1>
+            <h1>User Profile</h1>
             <?php
                 if (isset($successMessage)) echo "<p>" . $updateMessage . "</p>";
                 if (isset($errorMessage)) echo "<p>" . $errorMessage . "</p>";
                 else {
             ?>
                 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                    <p><strong>Username: </strong><?php echo $profileData['username']; ?></p>
+                    <p><strong>User: </strong><?php echo $profileData['username']; ?></p>
                     <p><label for="age">Age: </label><input type="number" id="age" name="age" value="<?php echo $profileData['age'] ?? ''; ?>"</p>
                     <p><label for="height">Height (in): </label><input type="number" id="height" name="height" value="<?php echo $profileData['height'] ?? ''; ?>"</p>
                     <p><label for="weight">Weight (lbs): </label><input type="number" id="weight" name="weight" value="<?php echo $profileData['weight'] ?? ''; ?>"</p>
